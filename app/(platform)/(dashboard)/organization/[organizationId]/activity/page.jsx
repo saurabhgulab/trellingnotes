835aa3e909842d3971f27c01f0cee91599b50630
Saurabh Krishan Gulab <saurabhgulab@gmail.com>
2024-10-20 21:01:31 +0530

    implemented prisma with cockroachdb

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/activity/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ActivityPage = () => {
+  return <div>This is ActivityPage</div>;
+};
+
+export default ActivityPage;
---
+++ app/(platform)/(dashboard)/organization/[organizationId]/billing/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BillingPage = () => {
+  return <div>This is billing page</div>;
+};
+
+export default BillingPage;
