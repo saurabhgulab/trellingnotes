8abc27af9758ba2ffa629c6272fd739808470ed3
Saurabh Krishan Gulab <saurabhgulab@gmail.com>
2024-10-21 20:47:22 +0530

    creating server actions for board creation

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,6 +1,23 @@
-// import { OrganizationSwitcher, auth } from "@clerk/nextjs";
+import React, { Suspense } from "react";
+import { Info } from "./_components/Info";
+import { Separator } from "@/components/ui/separator";
+import { BoardList } from "./_components/BoardList";
+import { checkSubscription } from "@/lib/subscription";
+
 const OrganizationIdPage = () => {
-  return <>Organization Page</>;
+  return (
+    <>
+      <div className="w-full mb-20">
+        <Info isPro={isPro} />
+        <Separator className="my-4" />
+        <div className="px-2 md:px-4">
+          <Suspense fallback={<BoardList.Skeleton />}>
+            <BoardList />
+          </Suspense>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default OrganizationIdPage;
