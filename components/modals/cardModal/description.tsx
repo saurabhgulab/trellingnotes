8ab987c8064b460bbad681e852c0e653a7344afe
Saurabh Krishan Gulab <saurabhgulab@gmail.com>
2024-10-28 20:55:15 +0530

    card component created successfully.

---
+++ components/modals/cardModal/description.tsx
@@ -0,0 +1,125 @@
+"use client";
+
+import { useState, useRef, ElementRef } from "react";
+import { AlignLeft } from "lucide-react";
+import { useEventListener, useOnClickOutside } from "usehooks-ts";
+import { useParams } from "next/navigation";
+import { useQueryClient } from "@tanstack/react-query";
+import { Skeleton } from "@/components/ui/skeleton";
+import { CardWithList } from "@/types";
+import { FormTextArea } from "@/components/forms/formTextArea";
+import FormSubmit from "@/components/forms/formSubmit";
+import { Button } from "@/components/ui/button";
+import { useAction } from "@/hooks/useAction";
+import { updateCard } from "@/actions/updateCard";
+import { toast } from "sonner";
+
+interface DescriptionProps {
+  data: CardWithList;
+}
+
+export const Description = ({ data }: DescriptionProps) => {
+  const queryClient = useQueryClient();
+  const params = useParams();
+  const [isEditing, setIsEditing] = useState(false);
+  const formRef = useRef<ElementRef<"form">>(null);
+  const textareaRef = useRef<ElementRef<"textarea">>(null);
+
+  const enableEditing = () => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    setIsEditing(true),
+      setTimeout(() => {
+        textareaRef.current?.focus();
+      });
+  };
+
+  const disableEditing = () => {
+    setIsEditing(false);
+  };
+
+  const onKeyDown = (e: KeyboardEvent) => {
+    if (e.key === "Escape") {
+      disableEditing();
+    }
+  };
+
+  useEventListener("keydown", onKeyDown);
+  useOnClickOutside(formRef, disableEditing);
+
+  const { execute, fieldErrors } = useAction(updateCard, {
+    onSuccess: (data) => {
+      // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+      queryClient.invalidateQueries({
+        queryKey: ["card", data.id],
+      }),
+        queryClient.invalidateQueries({
+          queryKey: ["card-logs", data.id],
+        });
+
+      toast.success(`Card "${data.title} updated"`);
+      disableEditing();
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
+
+  const onSubmit = (formData: FormData) => {
+    const description = formData.get("description") as string;
+    const boardId = params.boardId as string;
+    execute({ id: data.id, description, boardId });
+  };
+
+  return (
+    <div className="flex items-start gap-x-3 w-full">
+      <AlignLeft className="h-5 w-5 mt-0.5 text-neutral-700" />
+      <div className="w-full">
+        <p className="font-semibold text-neutral-700 mb-2">Description</p>
+        {isEditing ? (
+          <form action={onSubmit} ref={formRef} className="space-y-2">
+            <FormTextArea
+              id="description"
+              className="w-full mt-2"
+              placeholder="Add detailed description"
+              defaultValue={data.description || undefined}
+              errors={fieldErrors}
+              ref={textareaRef}
+            />
+            <div className="flex items-center gap-x-2">
+              <FormSubmit>Save</FormSubmit>
+              <Button
+                type="button"
+                onClick={disableEditing}
+                size="sm"
+                variant="ghost"
+              >
+                Cancel
+              </Button>
+            </div>
+          </form>
+        ) : (
+          <div
+            onClick={enableEditing}
+            role="button"
+            className="min-h-[78px] bg-neutral-200 text-sm font-medium py-3 
+          px-3.5 rounded-sm"
+          >
+            {data.description || "Add detailed description"}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+Description.Skeleton = function DescriptionSkeleton() {
+  return (
+    <div className="flex items-start gap-x-3 w-full">
+      <Skeleton className="h-6 w-6 bg-neutral-200" />
+      <div className="w-full">
+        <Skeleton className="w-24 h-6 mb-2 bg-neutral-200" />
+        <Skeleton className="w-full h-[78px] bg-neutral-200" />
+      </div>
+    </div>
+  );
+};
